11a5f7c2610bf90991c456230c7fe8accbf99874
Hassan Badru <hassan.badru@gmail.com>
2017-08-06 17:59:53 -0400

    view routing

--- rankings/urls.py
+++ rankings/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^view-product$', views.ProductView.as_view(), name='view-product'),
 ]
--- rankings/views.py
+++ rankings/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'review.html'
+
+class ProductView(TemplateView):
+    template_name = 'product.html'
